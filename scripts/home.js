0025dd18aaacc7b182ae644ce111828616d5a57b
brunocu <brunocu@msn.com>
2021-12-03 10:54:48 -0600

    global menu manager

--- scripts/home.js
+++ scripts/home.js
@@ -2,59 +2,19 @@
 // index.html script
 import { app } from "./index.js";
 // firebase
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js";
-import { collection, getDocs, getFirestore, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js";
+import { collection, getFirestore, onSnapshot } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js";
 
 // firebase app
-const auth = getAuth(app);
 const db = getFirestore(app);
 
 
 // DOM references
-const signOutButton = document.getElementById("home_signout");
 const cardsDiv = document.getElementById("cards");
 const quickSearch = document.getElementById("quick-search");
 const recipeModal = document.getElementById("recipeModal");
-// navbar elements
-const navbar = document.getElementById("navbar-nav");
-const navbarCol = navbar.children;
-const signOutNav = navbarCol[5];
-const signInNav = navbarCol[1];
 
 // helper objects
 const parser = new DOMParser();
-
-onAuthStateChanged(auth, async (user) => {
-    if (user) {
-        // User is signed in
-        // Enable signout
-        signOutNav.removeAttribute("hidden");
-        // Change signIn for user banner
-        signInNav.innerHTML = `
-        <a href="#" class="nav-link">
-            <i class="fas fa-user"></i>
-            <span class="link-text">${user.displayName}</span>
-        </a>
-        `;
-        // check for admin
-        const uid = user.uid;
-        const querySnapshot = await getDocs(query(collection(db, "admin_users"), where("u_id", "==", uid)));
-        if (!(querySnapshot.empty)) {
-            console.log("Admin account");
-            // add Admin panel nav
-            const adminNav = document.createElement("li");
-            adminNav.className = "nav-item";
-            adminNav.innerHTML = `
-            <a href="pages/index_admin.html" class="nav-link">
-                <i class="fas fa-toolbox"></i>
-                <span class="link-text">Admin panel</span>
-            </a>`;
-            signInNav.after(adminNav);
-        }
-    } else {
-        // User is signed out
-    }
-});
 
 // on doc load
 window.addEventListener("DOMContentLoaded", async () => {
@@ -139,19 +99,6 @@ function HTMLhighlight(text, regexp) {
     newText = newText.replace(regexp, '<mark>$&</mark>');
     return newText;
 }
-
-signOutButton.addEventListener("click", async (e) => {
-    signOut(auth)
-    .then(() => {
-        // sign-out successfull
-        console.log("User signed out");
-    })
-    .catch((error) => {
-        // an error happened
-        alert(error.message);
-        console.error(error);
-    })
-});
 
 // Scroll searchbar to top on focus
 quickSearch.querySelector("input").addEventListener("focus", async (e) => {
